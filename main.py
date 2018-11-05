10ed245b06539d53f1ac108e2077a896f05a6439
rchien6191 <44788760+rchien6191@users.noreply.github.com>
2018-11-05 14:30:42 -0800

    first push

---
+++ main.py
@@ -0,0 +1,56 @@
+#This file is by Robert Chien
+#Some material taken from KidsCanCode or Chris Bradfield
+
+import pygame as pg
+import random
+from settings import *
+from sprites import *
+
+class Game:
+    def __init__(self):
+        #init game window
+        #Init pygame
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("jumping")
+        self.clock = pg.time.Clock()
+        self.running = True
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.player = Player()
+        self.all_sprites.add(self.player)
+        self.run()
+        #New player element
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+        #Game loop
+    def update(self):
+        self.all_sprites.update()
+        #Update things
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing:
+                    selflplaying = False
+                self.running = False
+        #Listening for user input/events
+    def draw(self):
+        self.screen.fill(RED)
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+    def show_start_screen(self):
+        pass
+    def show_go (self):
+        pass
+g = Game()
+g.show_start_screen()
+
+while g.running:
+    g.new()
+    g.show_go_screen()
---
+++ settings.py
@@ -0,0 +1,8 @@
+TITLE = "Jump game"
+
+WIDTH = 480
+HEIGHT = 600
+FPS = 30
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+RED = (240, 55, 66)
